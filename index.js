08da69e4a2baa9171d7395cd5d22fbe14692b7be
unknown <ologydidza@gmail.com>
2024-03-22 10:47:40 +0200

    Added updated data

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import range from './range';
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults(range);
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-server.use(middlewares);
+server.use((req, res, next) => {
+    res.header("Content-Range", "posts 0-20/20")
+    next() // Continue to JSON Server Router   
+   });
+
 server.use(router);
 
 server.listen(port);
