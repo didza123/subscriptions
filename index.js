ce47afc5d0ad015e8009f4cb657be625ec173a5c
didza123 <107330521+didza123@users.noreply.github.com>
2024-03-25 14:13:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 server.use(middlewares);
 
 server.use((req, res, next) => {
-    //res.headers("Content-Range", "posts 0-20/20")
-    res.headers['X-Total-Count'] = '30' 
-   res.headers['Access-Control-Expose-Headers'] = 'Content-Range'
-   res.headers["Content-Range"] = "bytes: 0-9/*"
+    res.headers("Content-Range", "posts 0-20/20")
     next() // Continue to JSON Server Router   
    });
 
