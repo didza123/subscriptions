892d220eafd6e8464fcd6604ba5d35b4f95d45a0
didza123 <107330521+didza123@users.noreply.github.com>
2024-03-25 14:05:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+server.use(middlewares);
 
 server.use((req, res, next) => {
     res.header("Content-Range", "posts 0-20/20")
     next() // Continue to JSON Server Router   
    });
-
-server.use(middlewares);
 
 server.use(router);
 
